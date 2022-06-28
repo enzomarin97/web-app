85992cc70f86b66c6456bd10dfd6bf5124800ae6
Enzo Marin <enzzo.mariin@gmail.com>
2022-06-28 03:36:36 -0300

    navigation - home_page

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express(); //instanciando express
+
+//const rooms = ['general', 'games', 'futbol', 'study'];
+const cors = require('cors');
+const { Console } = require('console');
+
+app.use(express.urlencoded({extended:true})); //para recibir data del frontend
+app.use(express.json());
+app.use(cors());
+
+//crear el server
+const server = require('http').createServer(app);
+const PORT = 5001;
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'], 
+    }
+})
+//comunic back
+server.listen(PORT, ()=>{
+    console.log('listening to port', PORT);
+
+})
